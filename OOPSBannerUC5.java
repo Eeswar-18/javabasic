da7e57a97485f6227dd1869a97d4d41410dd2139
Eeswar-18 <reswar775@gmail.com>
2026-03-11 09:23:28 +0530

    Add files via upload

---
+++ OOPSBannerUC5.java
@@ -0,0 +1,26 @@
+/*
+ * OOPSBannerApp UC5
+ * Inline Array Initialization using String.join()
+ */
+
+public class OOPSBannerUC5{
+
+    public static void main(String[] args) {
+
+        String[] lines = {
+
+            String.join("   ", " ***** ", " ***** ", " ***** ", " ***** "),
+            String.join("   ", "*     *", "*     *", "*     *", "*     "),
+            String.join("   ", "*     *", "*     *", "*     *", "*     "),
+            String.join("   ", "*     *", "*     *", " ***** ", " ***** "),
+            String.join("   ", "*     *", "*     *", "*      ", "     * "),
+            String.join("   ", "*     *", "*     *", "*      ", "*     *"),
+            String.join("   ", " ***** ", " ***** ", "*      ", " ***** ")
+
+        };
+
+        for (String line : lines) {
+            System.out.println(line);
+        }
+    }
+}
